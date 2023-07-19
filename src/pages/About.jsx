963ec6455cbc2c23bddd1bda418de900df4feb04
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-19 08:54:08 +0700

    chore:updating about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
+import OurTeams from '../components/OurTeams';
+import Preface from '../components/Preface';
 
 function About() {
 	return (
 		<>
 			<h1>About Page</h1>
+			<Preface />
+			<OurTeams />
 			<Link to='/'>Back to Home</Link>
+			<br />
+			<Link to='/dsfsggf'>Not found page</Link>
 		</>
 	);
 }
