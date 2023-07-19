a28cd4fc390abff4df4813df62fd8f273172765d
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-19 08:52:35 +0700

    feat:register new routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,9 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Team from './pages/Team';
+import DetailTeam from './pages/DetailTeam';
 
 const router = createBrowserRouter([
 	{
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
 	{
 		path: '/about',
 		element: <About />,
+	},
+	{
+		path: '/team',
+		element: <Team />,
+	},
+	{
+		path: '/team/:id',
+		element: <DetailTeam />,
+	},
+	{
+		path: '*',
+		element: <NotFound />,
 	},
 ]);
 
